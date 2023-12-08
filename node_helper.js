8d3dfd8366b70d9784a0588439a59353680e4c08
dbeltjr <44065375+dbeltjr@users.noreply.github.com>
2023-12-08 17:02:24 -0500

    Update node_helper.js
    Updated for new node.js version

--- node_helper.js
+++ node_helper.js
@@ -5,13 +5,13 @@
  * MIT Licensed.
  */
 const NodeHelper = require("node_helper");
-const CalendarFetcher = require("./calendarfetcher.js");
 const Log = require("logger");
+const CalendarFetcher = require("./calendarfetcher");
 
 module.exports = NodeHelper.create({
 	// Override start method.
 	start: function () {
-		Log.log("Starting node helper for: " + this.name);
+		Log.log(`Starting node helper for: ${this.name}`);
 		this.fetchers = [];
 	},
 
@@ -19,13 +19,20 @@ module.exports = NodeHelper.create({
 	socketNotificationReceived: function (notification, payload) {
 		if (notification === "ADD_CALENDAR") {
 			this.createFetcher(payload.url, payload.fetchInterval, payload.excludedEvents, payload.maximumEntries, payload.maximumNumberOfDays, payload.auth, payload.broadcastPastEvents, payload.selfSignedCert, payload.id);
+		} else if (notification === "FETCH_CALENDAR") {
+			const key = payload.id + payload.url;
+			if (typeof this.fetchers[key] === "undefined") {
+				Log.error("Calendar Error. No fetcher exists with key: ", key);
+				this.sendSocketNotification("CALENDAR_ERROR", { error_type: "MODULE_ERROR_UNSPECIFIED" });
+				return;
+			}
+			this.fetchers[key].startFetch();
 		}
 	},
 
 	/**
 	 * Creates a fetcher for a new url if it doesn't exist yet.
 	 * Otherwise it reuses the existing one.
-	 *
 	 * @param {string} url The url of the calendar
 	 * @param {number} fetchInterval How often does the calendar needs to be fetched in ms
 	 * @param {string[]} excludedEvents An array of words / phrases from event titles that will be excluded from being shown.
@@ -47,7 +54,7 @@ module.exports = NodeHelper.create({
 
 		let fetcher;
 		if (typeof this.fetchers[identifier + url] === "undefined") {
-			Log.log("Create new calendarfetcher for url: " + url + " - Interval: " + fetchInterval);
+			Log.log(`Create new calendarfetcher for url: ${url} - Interval: ${fetchInterval}`);
 			fetcher = new CalendarFetcher(url, fetchInterval, excludedEvents, maximumEntries, maximumNumberOfDays, auth, broadcastPastEvents, selfSignedCert);
 
 			fetcher.onReceive((fetcher) => {
@@ -65,7 +72,7 @@ module.exports = NodeHelper.create({
 
 			this.fetchers[identifier + url] = fetcher;
 		} else {
-			Log.log("Use existing calendarfetcher for url: " + url);
+			Log.log(`Use existing calendarfetcher for url: ${url}`);
 			fetcher = this.fetchers[identifier + url];
 			fetcher.broadcastEvents();
 		}
@@ -86,3 +93,4 @@ module.exports = NodeHelper.create({
 		});
 	}
 });
+
